72be9852b2d395dcc674b6a4542d90b50f453286
ju1ius <jules.bernable@laposte.net>
2010-12-26 06:16:25 +0100

    refactored, added string output

---
+++ README.md
@@ -0,0 +1,19 @@
+mootools-zencoding
+==================
+
+Wraps zen_parser to generate Elements from zen-coding abbreviations.
+
+How to use
+----------
+
+    var abbr = 'table>(thead>tr>th{Row Header}*4)+(tfoot>tr>td[colspan=4])+(tbody>tr*4>td{Cell...}*4)';
+    
+    // Parse abbreviation and injects the resulting elements
+    $('my-element').expandAbbreviation(abbr);
+    // Or return an Elements object containing the generated elements
+    var els = ZenCoding.abbr_to_elements(abbr);
+    
+    // Parse abbreviation into an HTML string
+    var html = String.expandAbbreviation(abbr);
+    // Or
+    var html = ZenCoding.abbr_to_html(abbr);
--- Source/ZenCoding.js
+++ Source/ZenCoding.js
@@ -9,24 +9,28 @@ authors: Jules Bernable
 
 requires:
 - core/1.3:Element
-- https://github.com/sergeche/zen-coding/javascript/zen_parser.js
+- https://github.com/sergeche/zen-coding/blob/master/javascript/zen_parser.js
 
 provides: [ZenCoding, Element.expandAbbreviation]
 
 ...
 */
-ZenCoding = {
-  expand: function(abbr)
+(function()
+{
+  var unary_tags = [
+    'area','base','basefont','br','col','frame','hr','img','input',
+    'isindex','link','meta','param','embed','keygen','command'
+  ];
+  var is_empty = function(tag)
   {
-    var r = [], zen_tree = zen_parser.parse(abbr),
-      children = zen_tree.children, l = children.length;
-    for (var i = 0; i < l; i++)
-    {
-      r.push(this.parse_zen_node(children[i]));
-    };
-    return r;
-  },
-  parse_zen_node: function(node)
+    return (!tag.text && !tag.children.length) || unary_tags.contains(tag.name);
+  };
+  var indent = function(depth)
+  {
+    return new Array(depth+1).join('  ');
+  };
+
+  var zen_node_to_el = function(node)
   {
     var opts = {},
       attrs = node.attributes, al = attrs.length;
@@ -42,7 +46,7 @@ ZenCoding = {
     {
       for(var j = 0; j < cl; j++)
       {
-        parent.adopt(this.parse_zen_node(children[j]));
+        parent.adopt(zen_node_to_el(children[j]));
       }
     }
     if(node.count > 1)
@@ -55,11 +59,79 @@ ZenCoding = {
       return els
     }
     return parent;
-  } 
-};
+  }; 
+
+  var zen_node_to_html = function(node, depth)
+  {
+    var str = indent(depth) + '<' + node.name,
+      attrs = node.attributes, al = attrs.length,
+      u = is_empty(node);
+    
+    for (var i = 0; i < al; i++)
+    {
+      var attr = attrs[i];
+      str += ' ' + attr.name +'="' + attr.value + '"';
+    }
+    str += (u?'/':'') + '>' + "\n";
+    if(!u)
+    {
+      if(node.text) str +=  indent(depth+1) + node.text + "\n";
+    
+      var children = node.children, cl = children.length;
+      if(cl)
+      {
+        for(var j = 0; j < cl; j++)
+        {
+          str += zen_node_to_html(children[j], depth+1);
+        }
+      }
+      str += indent(depth) + '</'+node.name+'>' + "\n";
+    }
+    if(node.count > 1)
+    {
+      var clone = str;
+      for (var k = 0; k < node.count - 1; k++)
+      {
+        str += clone;
+      };
+    } 
+    return str;
+  };
+
+  ZenCoding = {};
+
+  ZenCoding.abbr_to_elements = function(abbr)
+  {
+    var r = [], zen_tree = zen_parser.parse(abbr),
+      children = zen_tree.children, l = children.length;
+    for (var i = 0; i < l; i++)
+    {
+      r.push(zen_node_to_el(children[i]));
+    };
+    return new Elements(r);
+  },
+  ZenCoding.abbr_to_html = function(abbr)
+  {
+    var r = '', zen_tree = zen_parser.parse(abbr),
+      children = zen_tree.children, l = children.length;
+    
+    for (var i = 0; i < l; i++)
+    {
+      r += zen_node_to_html(children[i], 0);
+    };
+    return r;
+  }
+
+})();
+
 Element.implement({
   expandAbbreviation: function(abbr)
   {
-    this.adopt(ZenCoding.expand(abbr));
+    this.adopt(ZenCoding.abbr_to_elements(abbr));
   }
 });
+
+String.expandAbbreviation = function(abbr)
+{
+  return ZenCoding.abbr_to_html(abbr);
+}
